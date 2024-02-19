4dd4bc37f46ab4299a68a16ec173390fa50fa083
Vlads <87931410+Vladislayyyyyy@users.noreply.github.com>
2024-02-19 21:51:44 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <cstdlib>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a;
+    int randomNum = rand() % 15 + 1;
+
+
+    while (a!=randomNum) {
+        cout << "Choose random number:"<< endl;
+        cin >> a;
+        if (a == randomNum) {
+            cout << "Yep, you're right";
+        } else
+            cout << "Nope,try again" << endl;
+    }
+
     return 0;
 }
